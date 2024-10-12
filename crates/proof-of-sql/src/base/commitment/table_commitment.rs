71cdc17be43ee770b21c620a38cd8a219fa4bb00
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2024-10-13 00:26:20 +0530

    add docs

--- crates/proof-of-sql/src/base/commitment/table_commitment.rs
+++ crates/proof-of-sql/src/base/commitment/table_commitment.rs
@@ -78,42 +78,6 @@ pub enum TableCommitmentArithmeticError {
     NonContiguous,
 }
 
-/// Errors that can occur when trying to create or extend a [`TableCommitment`] from a record batch.
-// #[cfg(feature = "arrow")]
-// #[derive(Debug, Snafu)]
-// pub enum RecordBatchToColumnsError {
-//     /// Error converting from arrow array
-//     #[snafu(transparent)]
-//     ArrowArrayToColumnConversionError {
-//         /// The underlying source error
-//         source: ArrowArrayToColumnConversionError,
-//     },
-//     #[snafu(transparent)]
-//     /// This error occurs when convering from a record batch name to an identifier fails. (Which may be impossible.)
-//     FieldParseFail {
-//         /// The underlying source error
-//         source: ParseError,
-//     },
-// }
-
-// /// Errors that can occur when attempting to append a record batch to a [`TableCommitment`].
-// #[cfg(feature = "arrow")]
-// #[derive(Debug, Snafu)]
-// pub enum AppendRecordBatchTableCommitmentError {
-//     /// During commitment operation, metadata indicates that operand tables cannot be the same.
-//     #[snafu(transparent)]
-//     ColumnCommitmentsMismatch {
-//         /// The underlying source error
-//         source: ColumnCommitmentsMismatch,
-//     },
-//     /// Error converting from arrow array
-//     #[snafu(transparent)]
-//     ArrowBatchToColumnError {
-//         /// The underlying source error
-//         source: RecordBatchToColumnsError,
-//     },
-// }
-
 /// Commitment for an entire table, with column and table metadata.
 ///
 /// Unlike [`ColumnCommitments`], all columns in this commitment must have the same length.
